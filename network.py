7f04fd835721d8c434bc6c5fbec5f70a6266f405
ifokthenok <ifokthenok@gmail.com>
2018-12-26 02:12:57 +0800

    MSE works

--- network.py
+++ network.py
@@ -36,13 +36,21 @@ class FooNet:
                 self.params['W' + str(k)] = self.layers[k].W
                 self.params['b' + str(k)] = self.layers[k].b
 
-    def train_one_batch(self, batch_x, batch_y):
+    def train_one_batch(self, batch_x, batch_y, loss_list=None, acc_list=None):
         x = batch_x
         for v in self.layers.values():
             x = v.forward(x)
+
         l = self.L.loss(batch_y, x)
         print('loss: ' + str(l))
-        
+        if not loss_list is None:
+            loss_list.append(l)
+
+        acc = self.accuracy(batch_x, batch_y)
+        print('acc: ' + str(acc))
+        if not acc_list is None:
+            acc_list.append(acc) 
+
         g = self.L.grad(batch_y, x)
         keys = list(self.layers.keys())
         keys.reverse()
@@ -58,7 +66,23 @@ class FooNet:
 
     def train(self, train_x, train_y, batch_size):
         steps = train_x.shape[0]//batch_size
+        loss_list = []
+        acc_list = []
         for i in range(steps):
             batch_x = train_x[i*batch_size:(i+1)*batch_size]
             batch_y = train_y[i*batch_size:(i+1)*batch_size]
-            self.train_one_batch(batch_x, batch_y)
+            self.train_one_batch(batch_x, batch_y, loss_list, acc_list)
+        return loss_list, acc_list
+    
+    def predict(self, x):
+        for v in self.layers.values():
+            x = v.forward(x)
+        return x
+    
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+        return np.sum(y == t) / float(x.shape[0])
+
+            
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import datasets.mnist as mnist
+import matplotlib.pyplot as plt
 import layers
 import losses
 import optimizers
@@ -11,21 +12,22 @@ if __name__ == '__main__':
     batch_x=train_x[:batch_size]
     batch_y=train_y[:batch_size]
 
-    scale = 0.05
+    scale = 0.01
     W1 = scale*np.random.rand(batch_x.shape[1], 100)
     b1 = scale*np.random.rand(100)
-    W2 = scale*np.random.rand(100, 200)
-    b2 = scale*np.random.rand(200)
-    W3 = scale*np.random.rand(200, 10)
-    b3 = scale*np.random.rand(10)
+    W2 = scale*np.random.rand(100, 10)
+    b2 = scale*np.random.rand(10)
 
     net = network.FooNet()
     net.add(layers.Affine(W1, b1), (batch_size, 100), input_shape=batch_x.shape)  
     net.add(layers.ReLU(), (batch_size, 100))
-    net.add(layers.Affine(W2, b2), (batch_x.shape[0], 200))  
-    net.add(layers.ReLU(), (batch_x.shape[0], 200)) 
-    net.add(layers.Affine(W3, b3), (batch_x.shape[0], 10)) 
-    net.add(layers.Softmax(), (batch_x.shape[0], 10))
+    net.add(layers.Affine(W2, b2), (batch_size, 10)) 
+    net.add(layers.Softmax(), (batch_size, 10))
     net.summary()
-    net.compile(losses.CrossEntropy(), optimizers.SGD())
-    net.train(train_x, train_y, batch_size)
+    net.compile(losses.MSE(), optimizers.SGD(lr=0.005))
+    loss_list, acc_list = net.train(train_x, train_y, batch_size)
+    x = range(len(loss_list))
+    plt.plot(x, loss_list)
+    plt.plot(x, acc_list)
+    plt.show()
+    
