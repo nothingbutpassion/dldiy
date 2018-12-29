9e73defd46e2a101c1109ada4888f930e5b3b696
yuhao <yuhao@hsaecd.com>
2018-12-29 18:26:50 +0800

    save changes

--- utils.py
+++ utils.py
@@ -33,13 +33,12 @@ def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
     # after reshape:    (N, out_h, out_w, C, filter_h, filter_w)
     # after transpose:  (N, C, filter_h, filter_W, out_h, out_w)
     col = col.reshape((N, out_h, out_w, C, filter_h, filter_w)).transpose(0, 3, 4, 5, 1, 2)
-    img = np.zeros((N, C, H + 2*pad + stride -1, W + 2*pad + stride -1))
+    img = np.zeros((N, C, H + 2*pad + stride - 1, W + 2*pad + stride - 1))
     for y in range(filter_h):
         y_max = y + stride * out_h
         for x in range(filter_w):
             x_max = x + stride * out_w
             img[:, :, y:y_max:stride, x:x_max:stride] = col[:, :, y, x, :, :]
-    
     return img[:, :, pad:pad+H + pad, pad:pad+W]
 
 
