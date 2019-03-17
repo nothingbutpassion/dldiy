3fa051f01dc170d1642433f0d05eead9e570f24f
ifokthenok <ifokthenok@gmail.com>
2019-03-17 17:03:58 +0800

    fix initializers

--- initializers.py
+++ initializers.py
@@ -6,7 +6,7 @@ def _he_normal(shape):
     A truncated normal distribution centered on 0 with "stddev = sqrt(2 / fan_in)"
     """
     fan_in = shape[0]
-    return np.random.rand(*shape) * np.sqrt(2.0/fan_in)
+    return np.random.randn(*shape) * np.sqrt(2.0/fan_in)
 
 
 def _lecun_normal(shape):
@@ -15,7 +15,11 @@ def _lecun_normal(shape):
     A truncated normal distribution centered on 0 with "stddev = sqrt(1 / fan_in)"
     """
     fan_in = shape[0]
-    return np.random.rand(*shape) * np.sqrt(1.0/fan_in)
+    return np.random.randn(*shape) * np.sqrt(1.0/fan_in)
+
+class StdNormal:
+    def __call__(self, shape):
+        return np.random.randn(*shape)
 
 class HeNormal:
     def __call__(self, shape):
