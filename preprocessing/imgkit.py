256ddb5a83d4dee98911890b0175dcb8157b0c48
unknown <yuhao@hsaecd.com>
2020-10-19 20:12:39 +0800

    clean code

--- preprocessing/imgkit.py
+++ preprocessing/imgkit.py
@@ -2,16 +2,6 @@
 import numpy as np
 from PIL import Image, ImageDraw
 
-def draw_grids(image, grid_shape, color=(255,0,0), width=2):
-    d = ImageDraw.Draw(image)
-    gw, gh = grid_shape
-    w, h = image.size[0], image.size[1]
-    rw, rh = w/gw, h/gh
-    for j in range(gh):
-        d.line([1, j*rh, w-1, j*rh], fill=color, width=width)
-    for i in range(gw):
-        d.line([i*rw, 1, i*rw, h-1], fill=color, width=width)
-
 def draw_boxes(image, boxes, color=(255,0,0), width=2):
     d = ImageDraw.Draw(image)
     for box in boxes:
